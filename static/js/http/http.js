0e55e61bde88cd121fa4f5a6f55da52f3cac1642
nd-r <andrey.kuchin97@gmail.com>
2017-09-17 00:17:11 +0300

    Пересмотрен http класс

--- static/js/http/http.js
+++ static/js/http/http.js
@@ -1,35 +1,48 @@
 'use strict';
 
 export class http {
+    static _server_path = '';
+
+    /**
+     * GET request to server
+     * @param path path of request
+     * @param callback function that handles xhr response and response body
+     */
     static get = (path, callback) => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.open('GET', path, true);
-        xhr.withCredentials = true;
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== 4) {
-                return;
-            }
-
-            if (+xhr.status >= 500) {
-                callback(xhr, {serverStatus: 'SERVER_DOWN'});
-            }
-
-            const response = JSON.parse(xhr.responseText);
-            response.serverStatus = 'OK';
-
-            callback(xhr, response);
-        };
-
-        xhr.send();
+        this._request('GET', path, null, callback);
     };
 
 
+    /**
+     * POST request to server.
+     * @param path path of request
+     * @param body body of request
+     * @param callback function that handles xhr response and response body
+     */
     static post = (path, body, callback) => {
+        this._request('POST', path, body, callback);
+    };
+
+    /**
+     * Request function sends request to server and handles it with callback
+     * @param method 'GET' or 'POST' method of request
+     * @param path path of request
+     * @param body body of request
+     * @param callback function that handles xhr response and response body
+     * @private
+     */
+    static _request = (method, path, body, callback) => {
+        if (method !== 'GET' && method !== 'POST' ||
+            typeof path !== "string" ||
+            typeof body !== "string" ||
+            typeof callback !== "function") {
+
+            return;
+        }
+
         const xhr = new XMLHttpRequest();
 
-        xhr.open('POST', path, true);
+        xhr.open(method, this._server_path + path, true);
         xhr.withCredentials = true;
 
         xhr.onreadystatechange = () => {
@@ -46,6 +59,10 @@ export class http {
             callback(xhr, response);
         };
 
-        xhr.send(body);
+        if (method === 'GET') {
+            xhr.send();
+        } else {
+            xhr.send(body);
+        }
     }
 }
