234167127295e1b2f310c3466be8e780110e99d5
nd-r <andrey.kuchin97@gmail.com>
2017-10-27 15:01:32 +0300

    rk2 fix

--- static/js/Online/views/lobbyView/lobbyView.ts
+++ static/js/Online/views/lobbyView/lobbyView.ts
@@ -1,4 +1,4 @@
-import {EventsEnum, ScopesEnum} from '../../../modules/eventBus';
+import { EventsEnum, ScopesEnum } from '../../../modules/eventBus';
 import BaseView from '../../../modules/baseView';
 import MapMeta from '../../../services/mapMeta';
 import mapService from '../../../services/mapService';
@@ -7,47 +7,49 @@ import Button from '../../../blocks/button';
 const lobbyTmpl = require('./lobbyView.pug') as TemplateRenderFunc;
 
 export default class LobbyView extends BaseView {
-  private maps: MapMeta[];
-  private mapTiles: Button[];
+    private maps: MapMeta[];
+    private mapTiles: Button[];
 
-  constructor(parentElement) {
-    super(parentElement);
+    constructor(parentElement) {
+        super(parentElement);
 
-    this.name = 'Lobby';
-    this.renderFunc = lobbyTmpl;
+        this.name = 'Lobby';
+        this.renderFunc = lobbyTmpl;
 
-    this.pageSection.id = 'lobby-page';
+        this.pageSection.id = 'lobby-page';
 
-  }
-
-  public start(): void {
-    this.maps = mapService.getMaps(true);
-
-    this.pageSection.innerHTML = this.renderFunc(this.maps);
-    this.rootElement.appendChild(this.pageSection);
-
-    const mapTileElements = this.rootElement.getElementsByClassName('lobby-online__container__map');
-    for (let i = 0; i < mapTileElements.length; i++) {
-      const button = new Button(mapTileElements[i] as HTMLElement);
-      button.onClick(() => {
-        this.bus.emit(ScopesEnum.online, EventsEnum.map_chosen, this.maps[i]);
-      });
-
-      this.mapTiles.push(button);
     }
 
-  }
+    public start(): void {
+        this.maps = mapService.getMaps(true);
 
-  public destroy(): void {
-    this.rootElement.removeChild(this.pageSection);
-    this.pageSection.innerHTML = '';
-  }
+        this.pageSection.innerHTML = this.renderFunc();
+        this.rootElement.appendChild(this.pageSection);
 
-  public resume(): void {
-    this.show();
-  }
+        const mapTileElements = this.rootElement.getElementsByClassName('lobby-online__container__map');
+        console.log(mapTileElements);
+        for (let i = 0; i < mapTileElements.length; i++) {
+            const button = new Button(mapTileElements[i] as HTMLElement);
+            button.onClick(() => {
+                console.log(1);
+                this.bus.emit(ScopesEnum.online, EventsEnum.map_chosen, this.maps[i]);
+            });
 
-  public pause(): void {
-    this.hide();
-  }
+            this.mapTiles.push(button);
+        }
+
+    }
+
+    public destroy(): void {
+        this.rootElement.removeChild(this.pageSection);
+        this.pageSection.innerHTML = '';
+    }
+
+    public resume(): void {
+        this.show();
+    }
+
+    public pause(): void {
+        this.hide();
+    }
 }
