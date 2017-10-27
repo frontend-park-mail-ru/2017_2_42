c1f2000124e0163af47debb03120ec586a460867
nd-r <andrey.kuchin97@gmail.com>
2017-10-27 16:40:56 +0300

    rk2 fix

--- static/js/services/mapService.ts
+++ static/js/services/mapService.ts
@@ -4,43 +4,42 @@ import http from '../modules/http';
 import Utils from '../modules/utils/utils';
 
 class MapService {
-  private mapMetaArr: MapMeta[];
+    private mapMetaArr: MapMeta[];
 
-  constructor() {
-    this.mapMetaArr = [];
-  }
-
-  // public getMap(id: number): Map {
-  //     http.prGet(`${PATHS.GET_MAP}/${id}`)
-  //         .then((data) => this.loadedMap = data as Map)
-  //         .catch((err) => {
-  //             this.loadedMap = undefined;
-  //             Utils.debugWarn(err)
-  //         });
-
-  //     return this.loadedMap;
-  // }
-
-  public getMaps(force?: boolean): MapMeta[] {
-    if (!force) {
-      return this.mapMetaArr;
+    constructor() {
+        this.mapMetaArr = [];
     }
-    else {
-      this.getMapsFromServer();
-      return this.mapMetaArr;
+
+    // public getMap(id: number): Map {
+    //     http.prGet(`${PATHS.GET_MAP}/${id}`)
+    //         .then((data) => this.loadedMap = data as Map)
+    //         .catch((err) => {
+    //             this.loadedMap = undefined;
+    //             Utils.debugWarn(err)
+    //         });
+
+    //     return this.loadedMap;
+    // }
+
+    public getMaps(force?: boolean): Promise<any> {
+        if (!force) {
+            return new Promise(() => this.mapMetaArr);
+        }
+        else {
+            return this.getMapsFromServer();
+        }
     }
-  }
 
-  private getMapsFromServer(sort?: string): void {
-    http.prGet(`${PATHS.GET_MAPS_PATH}?sort=${sort || 'rating'}`)
-      .then((data: any) => this.mapMetaArr = data as MapMeta[])
-      .catch((err) => {
-        this.mapMetaArr = undefined;
-        Utils.debugWarn(err);
-      });
-  }
+    private getMapsFromServer(sort?: string): Promise<any> {
+        return http.prGet(`${PATHS.GET_MAPS_PATH}?sort=${sort || 'rating'}`)
+            .then((data: any) => { this.mapMetaArr = data as MapMeta[]; return data })
+            .catch((err) => {
+                this.mapMetaArr = undefined;
+                Utils.debugWarn(err);
+            });
+    }
 
-  // private loadedMap: Map;
+    // private loadedMap: Map;
 }
 
 export default new MapService();
