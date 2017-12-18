c0a98f50284901e2760fcf5ad6e4e7ed91778d80
zwirec <zwirec@icloud.com>
2017-12-18 09:39:54 +0300

    update

--- src/js/views/Pages/gameView/gameView.ts
+++ src/js/views/Pages/gameView/gameView.ts
@@ -21,6 +21,7 @@ export default class GameView extends BaseView {
     private backButton: Button;
     private startButton: Button;
     private settingsButton: Button;
+    public canvasScale: number = 1;
     // private game: GameOnline;
 
     constructor(parentElement: HTMLElement) {
@@ -68,9 +69,44 @@ export default class GameView extends BaseView {
     private initCanvas(): HTMLCanvasElement {
         const canvas = document.querySelector('.main-frame__game-canvas') as HTMLCanvasElement;
         const canvasSize = this.chooseCanvasSize(canvas);
+
         window.onresize = (ev) => {
-            console.log('resize');
-            this.chooseCanvasSize(canvas);
+
+            let size = {width: (<HTMLDivElement>canvas.parentElement).offsetWidth, height: (<HTMLDivElement>canvas.parentElement).offsetHeight};
+            let c = game.board.canvas;
+
+            // canvas.width = size.width;
+            // canvas.height = size.height;
+
+            console.log(c.getHeight(), c.getWidth());
+
+            let widthScale = size.width / c.getWidth();
+            let heightScale = size.height / c.getHeight();
+
+            c.setHeight(size.height);
+            c.setWidth(size.width);
+
+            let objects = c.getObjects();
+            for (let i in objects) {
+                let scaleX = objects[i].scaleX;
+                let scaleY = objects[i].scaleY;
+                let left = objects[i].left;
+                let top = objects[i].top;
+
+                let tempScaleX = scaleX * widthScale;
+                let tempScaleY = scaleY * heightScale;
+                let tempLeft = left * widthScale;
+                let tempTop = top * heightScale;
+
+                objects[i].scaleX = tempScaleX;
+                objects[i].scaleY = tempScaleY;
+                objects[i].left = tempLeft;
+                objects[i].top = tempTop;
+
+                objects[i].setCoords();
+            }
+
+            game.board.canvas.renderAll();
         };
         canvas.width = canvasSize.width;
         canvas.height = canvasSize.height;
@@ -85,11 +121,14 @@ export default class GameView extends BaseView {
         this.settingsButton = new Button(document.querySelector('.main-frame__header__settings-button') as HTMLElement);
         this.settingsButton.onClick(() => this.router.showOverlay(ViewService.OverlayNames.application.settings));
 
-        this.readyButton = new Button(document.querySelector('.main-frame__header__ready-button__not-ready') as HTMLElement);
-        this.readyButton.onClick(() => {
-            eventBus.emit('game', 'subscribe');
-        });
+        // this.readyButton = new Button(document.querySelector('.main-frame__header__ready-button__not-ready') as HTMLElement);
+        // this.readyButton.onClick(() => {
+        //     eventBus.emit('game', 'subscribe');
+        // });
         this.startButton = new Button(document.querySelector('.main-frame__header__ready-button__ready') as HTMLElement);
+        if (this.startButton === undefined) {
+            console.log('OP');
+        }
         this.startButton.onClick(() => {
             eventBus.emit('game', 'start');
         });
