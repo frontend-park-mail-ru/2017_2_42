bc0c246ad23e9d9dc9965100c3a667faaf7bdaa2
nd-r <andrey.kuchin97@gmail.com>
2017-12-23 02:03:42 +0300

    Service workers fix me path

--- src/js/workers.js
+++ src/js/workers.js
@@ -21,46 +21,8 @@ const STATICS = [
   // '/api/game/map/58',
 ];
 
-//
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open('v1').then(function(cache) {
-//       return cache.addAll(STATICS)
-//         .catch((resp) => {
-//         console.log('Hello', resp);
-//       });
-//     })
-//   );
-// });
-//
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     fromNetwork(event.request.clone(), 5000)
-//       .catch(() => fromCache(event.request.clone())));
-// });
-//
-// const fromNetwork = (request, timeout) => {
-//   return new Promise((fulfill, reject) => {
-//     let timeoutId = setTimeout(reject, timeout);
-//     return fetch(request).then((response) => {
-//       clearTimeout(timeoutId);
-//       fulfill(response);
-//     });
-//   });
-// };
-//
-// self.addEventListener('activate', (event) => {
-//   console.log('V1 now ready to handle fetches!');
-// });
-//
-// const fromCache = (request) => {
-//   return caches.open('v1')
-//     .then((cache) => cache.match(request))
-//     .then((matching) => matching || Promise.reject('no-match'));
-// };
 
-
-const version = 'v3';
+const version = 'v4';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -86,11 +48,13 @@ self.addEventListener('fetch', function(event) {
         return response;
       }
       return fetch(event.request).then(function(response) {
-        let responseClone = response.clone();
+        if (path !== '/backend/api/auth/me') {
+          let responseClone = response.clone();
 
-        caches.open(version).then(function(cache) {
-          cache.put(event.request, responseClone);
-        });
+          caches.open(version).then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+        }
         return response;
       }).catch(function() {
         return Promise.reject('No-match');
